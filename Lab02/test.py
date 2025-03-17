e60a98bf6cc611d5c97b2d30fa30fb19b57c0c01
Andrii Petrunko <w68164@student.wsiz.edu.pl>
2025-03-17 11:09:17 +0100

    testing code

--- Lab02/test.py
+++ Lab02/test.py
@@ -27,3 +27,27 @@ print(g.attr)
 Klasa = type('Klasa', (Cos,), {'attr': 100})
 y = Klasa()
 print(y.__class__.__bases__)
+
+class Tam:
+    pass
+
+def f(obj):
+    return obj.age
+def nnw(cls):
+    x = object.__new__(cls)
+    x.attr = 13
+    x.age = 1
+    x.f = f
+    return x
+Tam.__new__ = nnw
+
+tam = Tam()
+print(tam.attr)
+print(tam.age)
+print(tam.f)
+
+class Meta(type):
+    def __new__(cls, name, bases, dct):
+        xx = super().__new__(cls, name, bases, dct)
+        xx.attr = 100
+        return xx
