3897878164f99526e8d39f1ecc7641ab96abdd1d
Andrii Petrunko <w68164@student.wsiz.edu.pl>
2025-04-08 12:50:03 +0200

    zadanie 7 lab 5

--- Lab05/zad07.py
+++ Lab05/zad07.py
@@ -1,13 +1,33 @@
-class Klasa1:
+class CRUD:
+    Object = None
+    def __init__(self, num):
+        self.num = num
     @staticmethod
-    def func1(a):
-        print(a)
-
+    def create():
+        return  "Created"
+    @staticmethod
+    def read():
+        return "Read"
+    @staticmethod
+    def update():
+        return "Updated"
+    @staticmethod
+    def delete():
+        return "Deleted"
     @classmethod
-    def func2(cls, a):
-        print(a)
+    def setObject_value(cls, value):
+        cls.Object = value
+        print("Object is set,", cls.Object)
+    @property
+    def num_squared(self):
+        return self.num ** 2
 
-Klasa1.func1(12)
-Klasa1.func2("1")
 
-
+print(CRUD.create())
+print(CRUD.read())
+print(CRUD.update())
+print(CRUD.delete())
+CRUD.setObject_value("Hello")
+C = CRUD(5)
+print(C.num)
+print(C.num_squared)
